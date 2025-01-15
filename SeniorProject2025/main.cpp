2566152a4ffcd3897604dbdf3092f11bb36e19a5
SSpectre4x <kaidenrobinson1@gmail.com>
2025-01-15 14:23:34 -0600

    Update main.cpp

--- SeniorProject2025/main.cpp
+++ SeniorProject2025/main.cpp
@@ -2,15 +2,36 @@
 
 #include <iostream>
 #include <iomanip>
+#include <array>
 using namespace std;
+
+float halsteadCalc(float);
 
 int main() {
 
 	cout << "Hello World!" << endl;
 
-	// Halstead Primitive
-	// 
+	// - Halstead Primitive -
+	// Program Length (N): N = N1 + N2
+	// Program Vocabulary (n): n1 + n2
+	// Volume (V): V = N * log(n) [if n>0]
+	// Difficulty (D): D = (n1 / 2) * (N2 / n2) [if n2>0]
+	// Effort (E): E = D * V
 
+	// values = {N1, N2, n1, n2}
+	float values[] = { 1, 1, 1, 1 };
+	float halstead = halsteadCalc(values);
+
+	cout << halstead[1] << endl;
 	
 	return 0;
+}
+
+float *halstead(float values) {
+
+	// result = {N, n, V, D, E}
+	float result[5];
+
+
+	return result;
 }
