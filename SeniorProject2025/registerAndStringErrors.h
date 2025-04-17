ee94cb6ef389293aef125fd6235eee61066b1162
Christopher Boggs <christopherbboggs@gmail.com>
2025-04-16 21:53:07 -0500

    Fix line endings in main.cpp. Add function definition for registerToString in registerToString.h

--- SeniorProject2025/registerAndStringErrors.h
+++ SeniorProject2025/registerAndStringErrors.h
@@ -11,6 +11,7 @@ namespace Error {
 }
 
 // Function declarations
+std::string registerToString(int reg);
 Error::Error* checkStringNewline(const std::string& line, int lineNum);
 std::vector<Error::Error> checkVolatileRegisters(const std::string& line, int lineNum, std::map<int, bool>& initializedRegs, std::map<int, bool>& wipedRegs);
 Error::Error* checkInputFormat(const std::string& line, int lineNum);
