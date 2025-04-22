79ddd08d632ae2271c8eddbc52f6eec3f7f79567
Christopher Boggs <christopherbboggs@gmail.com>
2025-04-22 16:11:28 -0500

    Fix

--- SeniorProject2025/main.cpp
+++ SeniorProject2025/main.cpp
@@ -614,7 +614,7 @@ int assembleAndLink(const string& file, QTextStream& out) {
     QProcess process;
     process.start(assembleCMD);
     process.waitForFinished();
-    out << QString::fromUtf8(process.readAllStandardOutput());
+    out << QString::fromUtf8(process.readAll());
     status = process.exitCode(); // assemble command
     if (status != 0) {
         out << "Assembly failed with error code: "
@@ -627,7 +627,7 @@ int assembleAndLink(const string& file, QTextStream& out) {
     QProcess process2;
     process2.start(linkCMD);
     process2.waitForFinished();
-    out << QString::fromUtf8(process2.readAllStandardOutput());
+    out << QString::fromUtf8(process2.readAll());
     status = process2.exitCode(); // assemble command
     if (status != 0) {
         out << "Linking failed with error code: "
